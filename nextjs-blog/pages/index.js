fbfe297bb482737d1f6de523fa7b43d4527b946c
Hatimnofear <hatimnofear@gmail.com>
2023-02-12 00:09:14 +0500

    learn
    learn coding

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,21 +1,31 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
+import Script from 'next/script';
 
-
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://connect.facebook.net/en_US/sdk.js" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
           Read <a href="/posts/first-post">this page!</a>
         </h1>
-
+< YourComponent />
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
